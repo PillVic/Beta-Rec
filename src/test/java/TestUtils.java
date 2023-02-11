fc4e4b768164e1afcf1d656d799f639eb4ac7fc1
neovic <16205111@hdu.edu.cn>
2023-02-11 15:02:10 +0800

    error log

--- src/test/java/TestUtils.java
+++ src/test/java/TestUtils.java
@@ -1,14 +1,23 @@
 import com.betarec.pojo.Movie;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.betarec.utils.ObjectAnalyzer.toJsonString;
 import static com.betarec.utils.ObjectAnalyzer.toJsonStringV2;
 
 public class TestUtils {
+    Logger logger = LoggerFactory.getLogger(TestUtils.class);
     @Test
     public void testToString(){
         Movie movie = new Movie("1,Toy Story (1995),Adventure|Animation|Children|Comedy|Fantasy");
         System.out.println(toJsonString(movie));
         System.out.println(toJsonStringV2(movie));
+    }
+
+    @Test
+    public void testLog(){
+        logger.info("hello info log");
+        logger.error("hello error log");
     }
 }
